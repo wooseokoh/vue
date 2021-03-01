7e5017c9c72269eec33bbd249663a30d52014a0c
wooseokoh <allsafe1206@gmail.com>
2021-03-01 17:39:32 +0900

    필요없는파일 제거

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+ Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore reprehenderit nam distinctio minus dignissimos consequuntur harum dolore nostrum? Iusto enim omnis maiores quaerat totam atque eius soluta neque veniam debitis!
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
 })
 </script>
 
 <style>
+html, body {
+  margin:0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
